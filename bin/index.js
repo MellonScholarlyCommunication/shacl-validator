b2aaf834e7e9388b5d51b2c22fb5d6a95a1409fc
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2026-03-05 15:37:43 +0100

    Fixing json schema validation

--- bin/index.js
+++ bin/index.js
@@ -118,6 +118,7 @@ program
   .command('server')
   .option('-c,--cache <contextCache>', 'local cache of JSON-LD contexts', process.env.CACHE)
   .option('-s,--shape <shapeFile>','shape file',process.env.SHAPE_FILE)
+  .option('--schema <schemaFile>','JSON schema file',process.env.SCHEMA_FILE)
   .option('--maxLength <size>','max size of an upload',Number(process.env.MAX_UPLOAD_SIZE))
   .option('--safe', 'load only context URLs from the cache')
   .option('--logging','Apache style logging',Boolean(process.env.LOGGING))
